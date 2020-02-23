de09b388acd1b4342216fb2ed553addc49a5a7db
Gergely Kalapos <gergo@kalapos.net>
2020-02-23 21:26:18 +0100

    Add demo app

---
+++ GrazMeetup_Observability_Samples/01_SampleApp/Currency.DbService/Controllers/ValuesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Currency.DbService.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Currency.DbService.Controllers
+{
+
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesController : ControllerBase
+	{
+		private SampleDbContext _context;
+		private Random _random = new Random();
+		ILogger<ValuesController> _logger;
+
+		public ValuesController(ILogger<ValuesController> logger, SampleDbContext dbContext)
+		{
+			_context = dbContext;
+			_logger = logger;
+
+			_context.Database.EnsureCreated();
+
+			if (_context.Sample.Count() == 0)
+			{
+				_context.Sample.Add(new Sample { Value = 10000000 });
+			}
+
+			_context.SaveChanges();
+		}
+
+		// GET api/values
+		[HttpGet]
+		public async Task<ActionResult<long>> Get()
+		{
+			var rnd = _random.Next(5);
+			_logger.LogInformation("Generated random number: {randomnumber}", rnd);
+
+			if (rnd == 0)
+			{
+				//randomly do work to generate high CPU usage
+				Prime(100000);
+			}
+
+			if (rnd == 1)
+			{
+				//randomly throw exceptions to generate errors
+				throw new Exception("Whuuuuuu");
+			}
+
+			var val = (await _context.Sample.FirstAsync()).Value;
+			return val;
+		}
+
+		private List<long> Prime(long num)
+		{
+			var retVal = new List<long>();
+
+			for (long i = 0; i <= num; i++)
+			{
+				bool isPrime = true;
+				for (long j = 2; j < i; j++)
+				{
+					if (i % j == 0)
+					{
+						isPrime = false;
+						break;
+					}
+				}
+				if (isPrime)
+				{
+					retVal.Add(i);
+				}
+			}
+
+			return retVal;
+		}
+
+		// GET api/values/5
+		[HttpGet("{id}")]
+		public ActionResult<string> Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/values
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/values/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
---
+++ GrazMeetup_Observability_Samples/01_SampleApp/Currency.DbService/Startup.cs
@@ -0,0 +1,50 @@
+using Currency.DbService.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Currency.DbService
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
+
+
+			services.AddDbContext<SampleDbContext>(options =>
+			   options.UseSqlServer(@"Data Source=DESKTOP-SK71JBF\SQLEXPRESS;Initial Catalog=SampleDb;Integrated Security=True"));
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
+}
